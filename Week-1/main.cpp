084c266ad61b4f453b85ab201ef8d7df002a5426
Jake Carlson <jrcarlson@smu.edu>
2018-08-20 19:40:55 -0500

    First class period

---
+++ Week-1/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * CSE 1342 - I/O and variables
+ * Jake Carlson
+ * 8/20/2018
+ */
+
+// include cout and cin
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // variables have two parts, a type and a name
+    int val1;
+    int val2;
+    
+    // cout     console output
+    // cin      console input
+    // >>       stream extraction operator
+    // <<       stream insertion  operator
+    cout << "Enter a number:";
+    cin >> val1;
+    cout << "Enter another number:";
+    cin >> val2;
+
+    // endl     write a new line
+    cout << val1 << " + " << val2 << " " << val1 + val2 << endl;
+    cout << val1 << " - " << val2 << " " << val1 - val2 << endl;
+    cout << val1 << " * " << val2 << " " << val1 * val2 << endl;
+    cout << val1 << " / " << val2 << " " << val1 / val2 << endl;
+
+    return 0;
+}
