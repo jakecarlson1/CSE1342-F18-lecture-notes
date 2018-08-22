7f50100ad3d7956ce695fa120d5c5b0c177f4851
Jake Carlson <jrcarlson@smu.edu>
2018-08-22 16:16:46 -0500

    Added base code for overflow demo

---
+++ Week-1/overflow.cpp
@@ -0,0 +1,21 @@
+/*
+ * CSE 1342 - Overflow and Assignment
+ * Jake Carlson
+ * 8/22/2018
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int big = 2048;
+    long bigger = big;
+    short small = big;
+
+    cout << "Integer:\t" << big << endl;
+    cout << "Long:\t\t" << bigger << endl;
+    cout << "Short:\t\t" << small << endl;
+    cout << "--------------------------------" << endl;
+
+    return 0;
+}
