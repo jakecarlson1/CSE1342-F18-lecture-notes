2eb0e91f87002c4751189ed8d2601f26996e12e4
Jake Carlson <jrcarlson@smu.edu>
2018-08-26 17:44:33 -0500

    Code for start of class Monday

---
+++ Week-2/fizzbuzz.cpp
@@ -0,0 +1,15 @@
+/*
+ * CSE 1342 - Fizz Buzz with If Else
+ * Jake Carlson
+ * 8/27/2018
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 100; i++) {
+    }
+
+    return 0;
+}
---
+++ Week-2/strings.cpp
@@ -0,0 +1,27 @@
+/*
+ * CSE 1342 - String Common Characters
+ * Jake Carlson
+ * 8/27/2018
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string word1;
+    string word2;
+    string commonChars;
+    
+    cout << "Enter the first word: ";
+    cin >> word1;
+    cout << "Enter the second word: ";
+    cin >> word2;
+
+    // find the common characters
+
+    cout << "The common characters are:" << endl;
+    cout << commonChars << endl;
+    
+    return 0;
+}
