e0307546afd250611f9ea5c50239087a98dbd111
Jake Carlson <jrcarlson@smu.edu>
2018-08-28 09:23:11 -0500

    Updated string code

--- Week-2/strings.cpp
+++ Week-2/strings.cpp
@@ -19,6 +19,22 @@ int main()
     cin >> word2;
 
     // find the common characters
+	for (int i = 0; i < word1.length(); i++) {
+		for (int j = 0; j < word2.length(); j++) {
+			if (word1.at(i) == word2.at(j)) {
+				commonChars += word1.at(i);
+			}
+		}
+	}
+
+	// remove duplicates
+	for (int i = 0; i < commonChars.length(); i++) {
+		for (int j = i + 1; j < commonChars.length(); j++) {
+			if (commonChars[i] == commonChars[j]) {
+				// remove the character at j
+			}
+		}
+	}
 
     cout << "The common characters are:" << endl;
     cout << commonChars << endl;
