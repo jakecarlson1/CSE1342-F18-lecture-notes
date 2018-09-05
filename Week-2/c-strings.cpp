c72bcb376340187e77329e0c4cab6fe2bfa4f3b1
Jake Carlson <jrcarlson@smu.edu>
2018-09-05 18:06:13 -0500

    Add c-string test case

--- Week-2/c-strings.cpp
+++ Week-2/c-strings.cpp
@@ -1,16 +1,23 @@
 /*
- *
- *
- *
+ * CSE 1342 - C-Strings
+ * Jake Carlson
+ * 9/5/2018
  */
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+int ourStringLen(char[]);
 
 int main()
 {
     char greeting[16] = "Hello!";
 
     cout << greeting << endl;
+
+	if (strlen(greeting) == ourStringLen(greeting)) {
+		cout << "Strlen passed" << endl;
+	}
 
     return 0;
 }
